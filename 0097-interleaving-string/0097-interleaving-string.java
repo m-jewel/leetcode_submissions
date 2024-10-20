f17f3f066a16667a5014de87a705aa93f437dbbd
Jewel Crystel Magcawas <145354608+m-jewel@users.noreply.github.com>
2024-10-20 15:04:40 -0600

    0097-INTERLEAVING-STRING Stats: Time: 7 ms (30.84%), Space: 41.5 MB (83.69%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,34 +1,41 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        int m = s1.length();
-        int n = s2.length();
+        int m = s1.length(), n = s2.length();
         
         // If the total length of s1 and s2 is not equal to s3, return false
         if (m + n != s3.length()) {
             return false;
         }
         
-        boolean[] dp = new boolean[n + 1];
-        // Initialize the DP array for the base case (when s1 is empty)
-        dp[0] = true;
+        boolean[][] dp = new boolean[m + 1][n + 1];
         
-        // Populate the DP array for the case where s1 is empty and s2 is compared with s3
-        for (int j = 1; j <= n; j++) {
-            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
-        }
-
-        // Traverse over s1 and update the DP array based on matches with s3
+        // Base case: dp[0][0] = true, since empty s1 and s2 interleave to form empty s3
+        dp[0][0] = true;
+        
+        // Fill the first row (when s2 is empty)
         for (int i = 1; i <= m; i++) {
-            // Update dp[0] for the case where s2 is empty and s1 is compared with s3
-            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
-
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+        }
+        
+        // Fill the first column (when s1 is empty)
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+        
+        // Fill the rest of the DP table
+        for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                // Check if the current character in s3 matches either from s1 or s2
-                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) ||
-                        (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                // If the character from s1 matches the current character in s3
+                if (s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+                // If the character from s2 matches the current character in s3
+                if (s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = dp[i][j] || dp[i][j - 1];
+                }
             }
         }
         
-        return dp[n];
+        return dp[m][n];
     }
 }
