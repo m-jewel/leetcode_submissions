36f0d17a681185271294c78c4b1f37f887756d88
Jewel Crystel Magcawas <145354608+m-jewel@users.noreply.github.com>
2024-10-22 16:37:09 -0600

    0806-DOMINO-AND-TROMINO-TILING Stats: Time: 1 ms (37.58%), Space: 41 MB (11.45%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -2,25 +2,27 @@ class Solution {
     public int numTilings(int n) {
         int MOD = 1_000_000_007;
 
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        if (n == 3) return 5;
+        // Base Cases
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        if(n == 3) return 5;
 
         long[] dp = new long[n + 1];
-        long[] g = new long[n + 1];
-        
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
+        long[] g = new long[n + 1]; // For tromino overlaps multiple tiles
+
+        dp[1] = 1; // Only one way to tile a 2 x 1 board
+        dp[2] = 2; // Either two vertical dominoes or two horizontal dominoes
+        dp[3] = 5; // Example 1
         
         g[2] = 1;
         g[3] = 2;
 
-        for (int i = 4; i <= n; i++) {
+        for(int i = 4; i <= n; i++){
+            // Place a vertical domino + two horizontal dominoes + trominoes which has two orientations hence times by two
             dp[i] = (dp[i - 1] + dp[i - 2] + 2 * g[i - 1]) % MOD;
-            g[i] = (g[i - 1] + dp[i - 2]) % MOD;
+            g[i] = (g[i - 1] + dp[i - 2]) % MOD; // Count both partial and full tilings for trominoes
         }
 
-        return (int) dp[n];
+        return (int)dp[n];
     }
 }
